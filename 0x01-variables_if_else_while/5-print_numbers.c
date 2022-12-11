3791f5fd26571a0f50e2cfa6cc7d046690d9fda1
sam1gate <samgatemul@gmail.com>
2022-12-11 14:35:03 +0300

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int numb;
+
+for (numb = 0; numb < 10; numb++)
+{
+printf("%d", numb);
+}
+printf("\n");
+
+return (0);
+
+}
