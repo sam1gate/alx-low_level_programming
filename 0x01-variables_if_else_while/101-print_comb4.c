391f68e8c72488b8fdbc8051ca166e8360129b6a
sam1gate <117744858+sam1gate@users.noreply.github.com>
2022-12-11 15:24:38 +0300

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
- **/
-
+ */
 int main(void)
 {
 
-int i, j, k;
+	int i, j, k;
 
-for (i = 48; i < 58; i++)
-{
-	for (j = i; j < 58; j++)							{											for (k = j; k < 58; k++)
-		{											if (i == j || j == k || i == k)	
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i; j < 58; j++)
+		{
+			for (k = j; k < 58; k++)
 			{
-				continue;
+				if (i == j || j == k || i == k)
+				{
+					continue;
+				}
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if (i == 55 && j == 56 && k == 57)
+				{
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
 			}
-			
-			putchar(i);									putchar(j);
-			putchar(k);																			if (i == 55 && j == 56 && k == 57)
-			{
-				break;									}
-			else				
-			{
-				putchar(',');
-				putchar(' ');
-			}
-												} 
+		}
 	}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
